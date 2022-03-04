49aee2ac08e63a318a242dda9d11beb15af336d4
Anastasiya Zaytseva <anastasiya.i.zaytseva@yandex.ru>
2022-03-04 22:30:59 +0300

    redact section and js

--- styles/scripts/script.js
+++ styles/scripts/script.js
@@ -1,18 +1,14 @@
-const btnRequestHeader = document.querySelector('.header__form-btn-save');
-const inputHeader = document.querySelector('.header__form-input');
-const btnRequestFooter = document.querySelector('.footer__form-btn-save');
-const inputFooter = document.querySelector('.footer__form-input');
+const btnRequestForm = document.querySelector('.form__btn-save');
+const inputForm = document.querySelector('.form__input');
 const btnNewText = 'Круто, спасибо за доверие!';
 
 function btnSave(button, input) {
     button.textContent = btnNewText;
+    inputForm.value = "";
     //открыть форму и поставить переменную текст из переменной input
 }
 
-btnRequestHeader.addEventListener('click', function(evt) {
-    btnSave (evt.target, inputHeader);
-})
-
-// btnRequestFooter.addEventListener('click', function(evt) {
-//     btnSave (evt.target, inputFooter);
-// })
+btnRequestForm.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    btnSave (evt.target, inputForm);    
+});
